34ba271078429480b854b93b934c1ed36f466fba
FedericoPaoletti <FedericoPaoletti@users.noreply.github.com>
2015-07-01 13:47:30 +0000

    Delete ReduceToF-generated.py

--- ReduceToF-generated.py
+++
@@ -1,6 +0,0 @@
-def ReduceToFgenerated(RPsets, final_closure):
-	for reactiontuple in RPsets:
-		if not reactiontuple[0].issubset(final_closure):
-			RPsets.remove(reactiontuple)
-	return RPsets
-			
